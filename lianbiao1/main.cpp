4d73e6553930681049c585af192bf7df7fdc52b5
20141105052 <xiaoods3on1@sina.com>
2015-12-09 18:10:25 +0800

    Initial Commit

---
+++ lianbiao1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lianbiao1
+//
+//  Created by 20141105052 on 15/12/9.
+//  Copyright (c) 2015年 20141105052郭孝宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
