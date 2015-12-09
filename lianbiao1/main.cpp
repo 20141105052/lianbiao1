fddbedefc6404b270d59339a7ecb6fd9aff9eefc
20141105052 <xiaoods3on1@sina.com>
2015-12-09 18:41:18 +0800

    简单的单向链表
    通过利用单向链表结构来输出学生数据，使用链表结构可以克服数组链表需要预先知道数据大小的缺点，链表结构可以充分利用计算机内存空间，实现灵活的内存动
    态管理。

--- lianbiao1/main.cpp
+++ lianbiao1/main.cpp
@@ -7,9 +7,38 @@
 //
 
 #include <iostream>
+struct student
+{
+    char name[100];
+    //char sex;
+    int age;
+    student *next;
+};
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    student A,B,C,D;
+    student *p,*head;
+    head=new student;
+    p=head;
+    scanf("%s%d",A.name,&A.age);
+    scanf("%s%d",B.name,&B.age);
+    scanf("%s%d",C.name,&C.age);
+    scanf("%s%d",D.name,&D.age);
+    A.next=&B;
+    B.next=&C;
+    C.next=&D;
+    D.next=0;
+    /*printf("%s %d\n",A.name,A.age);
+    printf("%s %d\n",B.name,B.age);
+    printf("%s %d\n",C.name,C.age);
+    printf("%s %d\n",D.name,D.age);*/
+    head=&A;
+    p=head;
+    while(p!=0)
+    {
+        printf("%s %d\n",p->name,p->age);
+        p=p->next;
+    }
     return 0;
 }
